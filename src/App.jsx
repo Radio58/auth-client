0944b9d0081b8b23dcb07581af2a5bcb99394a3c
Radio58 <rt58gm@gmail.com>
2024-01-26 15:32:24 +0000

    added register and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import Login from './components/login';
+import Register from './components/register'
 
 export default function App() {
   return (
     <div className="App">
-
+      <Register/>
+      <Login/>
     </div>
   );
 }
